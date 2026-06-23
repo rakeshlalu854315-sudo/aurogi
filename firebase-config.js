c050a50b3a003037171365e4852d538ae1612d83
rakeshlalu854315-sudo <rakeshlalu854315@gmail.com>
2026-06-23 21:42:18 +0530

    Add Firebase login and profile database

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyC09axE58FQYYbHYpEbQAv-Jx1cGD_S3c",
+  authDomain: "studio-3220983334-a2b03.firebaseapp.com",
+  projectId: "studio-3220983334-a2b03",
+  storageBucket: "studio-3220983334-a2b03.appspot.com",
+  messagingSenderId: "280762476817",
+  appId: "1:280762476817:web:38e86def7f77ca89f984b0"
+};
+
+export const app = initializeApp(firebaseConfig);
